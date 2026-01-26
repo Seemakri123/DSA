45bb0fac83f0c72a346ecbe3615276e6ffb3ee1b
Seema Kumari <seema.kri870@gmail.com>
2026-01-26 23:29:30 +0530

    added

---
+++ Array/checkIfArrayIsSorted.java
@@ -0,0 +1,58 @@
+package Array;
+
+import java.util.Scanner;
+
+public class checkIfArrayIsSorted {
+    
+    public static int checkIfArrayIsSortedOrNotInAsc(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+
+            {
+                if(arr[i]>arr[j])
+                {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+
+    public static int checkIfArrayIsSortedOrNotInDesc(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]<arr[j])
+                {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+    public static boolean checkIfArrayIsSortedOrNot(int arr[])
+    {
+        if(checkIfArrayIsSortedOrNotInAsc(arr)==1 || checkIfArrayIsSortedOrNotInDesc(arr)==1)
+        {
+           return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size=sc.nextInt();
+        System.out.println("Enter the element of the aaray : ");
+        int arr[] = new int[size];
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println(checkIfArrayIsSortedOrNot(arr)?"Sorted":"Not Sorted");
+    }
+}
