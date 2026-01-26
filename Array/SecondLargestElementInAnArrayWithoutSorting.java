e5515982b16995042a2f57ad292e266197d48c58
Seema Kumari <seema.kri870@gmail.com>
2026-01-26 23:18:45 +0530

    added

---
+++ Array/SecondLargestElementInAnArrayWithoutSorting.java
@@ -0,0 +1,38 @@
+package Array;
+
+import java.util.Scanner;
+
+public class SecondLargestElementInAnArrayWithoutSorting {
+    
+    public static int secondLargestElement(int[] arr)
+    {
+        int max =0;
+        int second=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(max<arr[i])
+            {
+                second=max;
+                max=arr[i];  
+            }
+            if(arr[i]>second && arr[i]!= max)
+            {
+                second=arr[i];
+            }
+        }
+        return second;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size=sc.nextInt();
+        System.out.println("Enter the element of the aaray : ");
+        int arr[] = new int[size];
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println(secondLargestElement(arr));
+    }
+}
